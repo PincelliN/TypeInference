fc427f4661813b1292f69cba986ba9dcf4d41780
Nicolò Pincelli <pincelli.n@gmail.com>
2024-12-11 18:03:21 +0100

    Utilizzo dei type  float bool e inference

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+float f = 12.5f;
+
+double d = 130.78;
+
+decimal dm = 1500.456m;
+
+double ne = 5e-7;
+
+//type inference  attraverso var lascia il compito di riconoscere il tipo di variabile attravesto la sua valorizzazione
+//stringa
+var s = "type inference";
+//boolean
+var b = false;
+
+
+Console.WriteLine($"Float={f}, Double={d}, Decimal={dm}, Double scentifico={ne}, InferenzaSring={s}, InferenzaBool={b}");
